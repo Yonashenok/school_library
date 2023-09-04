1c1564a2a7825a8e7436740b61885f057616ff51
Yonashenok <yonashenok24@gmail.com>
2023-09-04 16:53:52 +0300

    feat: Decorator class

---
+++ decorator.rb
@@ -0,0 +1,12 @@
+require './nameable'
+# class with method return instance variable
+class Decorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
