e19ef9f5dda59e0db92b6bfdb97bf0385d133355
Yonashenok <yonashenok24@gmail.com>
2023-09-04 12:35:23 +0300

    feat: teacher class is created

---
+++ teacher.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# teacher with there specialization and servies
+class Teacher < Person
+  def initialize(_specialization)
+    super()
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
