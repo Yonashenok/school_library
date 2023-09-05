8a3b872b94c258a9245df4c644d527dbeb1f56e2
Yonashenok <yonashenok24@gmail.com>
2023-09-05 09:57:36 +0300

    feat: create belongs to relation book and rantal

---
+++ book.rb
@@ -0,0 +1,11 @@
+# book for rental
+class Book
+  attr_reader :rentals
+  attr_accessor :title, :author
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+end
