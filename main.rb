d27d1b0d02cf414f98949bd7f7bcb22333e7415c
Yonashenok <yonashenok24@gmail.com>
2023-09-06 10:58:41 +0300

    fix alphabatic order

--- main.rb
+++ main.rb
@@ -7,18 +7,18 @@ def switch_case(number)
   case number
   when '1'
     puts ''
-    45.times { print '='}
+    45.times { print '=' }
     puts ''
     app.list_of_book
-    45.times { print '='}
+    45.times { print '=' }
     puts ''
-    
+
   when '2'
     puts ''
-    45.times { print '='}
+    45.times { print '=' }
     puts ''
     app.list_of_person
-    45.times { print '='}
+    45.times { print '=' }
     puts ''
   when '3'
     puts 'Do you went to create a student (1) or a teacher (2)? [Input the number]:'
@@ -32,10 +32,10 @@ def switch_case(number)
     puts 'Rental created succesfully'
   when '6'
     puts ''
-    45.times { print '='}
+    45.times { print '=' }
     puts ''
     app.rented_books
-    45.times { print '='}
+    45.times { print '=' }
     puts ''
   else
     puts puts 'wrong choose of number please select the right number from the list'
