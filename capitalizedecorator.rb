32db33df0c38fa948f69dccd5f8eddbfe9625378
Yonashenok <yonashenok24@gmail.com>
2023-09-13 12:17:22 +0300

    fix:  Unnecessary frozen string literal comment

--- capitalizedecorator.rb
+++ capitalizedecorator.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 require './decorator'
 # class that capitalizes given word
--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # nameable  class check its correct name
 class Nameable
   def correct_name
