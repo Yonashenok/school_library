69944aca25b61621d7e451ca9bcaf0c107694d81
DevaWinner <winneraaniekan@gmail.com>
2023-09-13 09:21:35 +0100

    Feature: Create file checker

---
+++ filechecker.rb
@@ -0,0 +1,10 @@
+class FileChecker
+  def self.read_json_file(file_path)
+    raise "File not found: #{file_path}" unless File.exist?(file_path)
+    File.read(file_path)
+  end
+
+  def self.file_exists?(file_path)
+    File.exist?(file_path)
+  end
+end
