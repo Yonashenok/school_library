28119ffadd4dbe709f1ede5cc601edce896155d2
DevaWinner <winneraaniekan@gmail.com>
2023-09-14 13:51:11 +0100

    Introduce @@all_students class variable to student

--- student.rb
+++ student.rb
@@ -1,21 +1,25 @@
 require './person'
+
 # student with there classroom and skills
 class Student < Person
   attr_reader :classroom
   attr_accessor :parent_permission
 
+  @@all_students = []
+
   def initialize(age, parent_permission, name = 'Unknown')
     super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
+    @classroom = nil
+    @@all_students << self
   end
 
   def self.all
-    ObjectSpace.each_object(self).to_a
+    @@all_students
   end
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
+    classroom.students << self unless classroom.students.include?(self)
   end
 
   def play_hooky
