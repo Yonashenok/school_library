fd7e70e726235d13feafc6111733f6cfab9a956a
DevaWinner <winneraaniekan@gmail.com>
2023-09-14 13:39:14 +0100

    Test: Declare before and add initialize test

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -7,4 +7,17 @@ describe Teacher do
   let(:parent_permission) { true }
   let(:teacher) { Teacher.new(specialization, age, name, parent_permission: parent_permission) }
 
+  before do
+    Teacher.class_variable_set(:@@all_teachers, []) # Reset the @@all_teachers array before each test
+  end
+
+  describe '#initialize' do
+    it 'sets the specialization, age, name, and parent_permission correctly' do
+      expect(teacher.specialization).to eq(specialization)
+      expect(teacher.age).to eq(age)
+      expect(teacher.name).to eq(name)
+      expect(teacher.parent_permission).to eq(parent_permission)
+    end
+  end
+
 end
